fa78b20cca963e4bf59ab5bd8d53d46cc98bb0e9
Derek Cross <derekcross@Leos-MacBook-Pro.local>
2019-10-10 18:23:32 -0400

    fixes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 
 const app = express();
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 const path = require("path");
-const PORT = process.env.PORT;
-const localRouter = require('./routes/local.route');
+const PORT = '3000'
+// const localRouter = require('./routes/local.route');
 
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const userController = require('./controllers/user.controller');
+const dogController = require('./controllers/dogs.controller');
 
+//USER REQUESTS / RESPONSES
+app.get('/user', userController.getUsers, (req, res) => {
+  res.status(200).json(res.locals.users);
+});
+
+//DOG REQUESTS / RESPONSES
+app.get('/dog', dogController.getDogs, (req, res) => {
+  res.status(200).json(res.locals.dogs);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
@@ -17,7 +28,7 @@ app.get("/", (req, res) => {
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-app.use('/', localRouter);
+// app.use('/', localRouter);
 
 app.use('*', (req, res) => {
   res.status(404).send("404 Page Not Found")
